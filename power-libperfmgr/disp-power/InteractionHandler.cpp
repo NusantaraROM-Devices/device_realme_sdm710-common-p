1661136c90c401537249f1e00d4387e85ed33b49
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 11:44:40 +0530

    sdm710-common: power-libperfmgr: Adapt and rebrand for sdm710
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: Heinz Der Flugel <heinzdf@gabuterscape.my.id>

--- power-libperfmgr/disp-power/InteractionHandler.cpp
+++ power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.sdm710-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>
@@ -33,8 +33,7 @@
 #define MSINSEC 1000L
 #define USINMS 1000000L
 
-static const std::vector<std::string> fb_idle_patch = {"/sys/class/drm/card0/device/idle_state",
-                                                       "/sys/class/graphics/fb0/idle_state"};
+static const std::vector<std::string> fb_idle_patch = {"/sys/class/drm/card0/device/idle_state"};
 
 InteractionHandler::InteractionHandler(std::shared_ptr<HintManager> const &hint_manager)
     : mState(INTERACTION_STATE_UNINITIALIZED),
