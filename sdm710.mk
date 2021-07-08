2b919a7256b70329c1d5b0992fe14fa6d01502eb
SamarV-121 <samarvispute121@gmail.com>
2021-07-08 20:02:15 +0200

    sdm710-common: Add powerhint for sdm710
     * Imported from bonito
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-byL KrishnakantShedge <shedgekrishnakant33@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -387,6 +387,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/sdm710_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
