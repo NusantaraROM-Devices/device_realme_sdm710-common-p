7c903900820ca336ebb07fc9f2725d6bde478303
danascape <danascape@gmail.com>
2021-10-11 17:14:18 +0200

    sdm710-common: build hidl vendor packages * Android S nuked them

--- sdm710.mk
+++ sdm710.mk
@@ -16,6 +16,9 @@ $(call inherit-product, external/json-c/Android.configure.mk)
 PRODUCT_PACKAGES += \
     libjson
 
+# HIDL 
+$(call inherit-product, $(LOCAL_PATH)/sdm710-hidl.mk)
+
 # Set dalvik vm config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
