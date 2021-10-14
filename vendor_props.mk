1a340ff3f570cac36af3b59204149214501b9256
Flex1911 <dedsa2002@gmail.com>
2021-10-14 10:01:49 +0200

    sdm710-common: Set USB config extra property
    * It should be persist.vendor.usb.config.extra, not persist.vendor.sys.usb.config.extra
    
    Change-Id: Icc5531c5563f1975314be38f5f7567e1c0f5eb8d

--- vendor_props.mk
+++ vendor_props.mk
@@ -192,3 +192,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Zygote preforking
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.usb.config.extra=adb
